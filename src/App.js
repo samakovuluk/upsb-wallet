7f8b801bff4ff8bae54dda20498cc03e7c63d35c
Куулар Бурбу Омакович <kuular.burbu@otr.ru>
2021-06-06 10:29:55 +0300

    Кошелек

--- src/App.js
+++ src/App.js
@@ -4,18 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is UPSB Wallet
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        КОШЕЛЕК
     </div>
   );
 }
